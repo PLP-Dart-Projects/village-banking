9e0e28b0f575a7d772f23ad10dcf6448e59f4041
Ridhiwan <ridhiwanmseya@gmail.com>
2023-06-16 22:59:32 +0300

    Elements of the home page and the bottom navigation bar have been added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -68,17 +68,122 @@ class _HomePageState extends State<HomePage> {
                     color: Color.fromRGBO(255, 255, 255, 1),
                     ),
                   ),
-                  const Text('Maria,',
+                  const Text('Zakia,',
                   style: TextStyle(
                     fontSize: 30,
                     color: Color.fromRGBO(255, 255, 255, 1),
                   ),
                   ),
-
                 ],
               ),
             ),
-          )
+          ),
+          Positioned(
+            top: screenHeight/2.75,
+            left: screenWidth/19,
+            right: screenWidth/19,
+            height: screenHeight/4.5,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: const Color.fromRGBO(255, 253, 253, 1),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.1),
+                    offset: Offset(0,1),
+                    blurRadius: 3,
+                  ),
+                ]
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 50,bottom: 10,left: 25,right: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Your total balance:',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color.fromRGBO(58, 58, 58, 1),
+                      ),
+                    ),
+                    SizedBox(
+                      height: screenHeight/26,),
+                    const Text('TSH  50,000',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(45, 153, 255, 1),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: screenHeight/1.64,
+            left: screenWidth/19,
+            right: screenWidth/19,
+            height: screenHeight/6,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: const Color.fromRGBO(73, 96, 249, 1),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromRGBO(27, 57, 255, 0.2),
+                    offset: Offset(0,1),
+                    spreadRadius: 3,
+                    blurRadius: 3,
+                  ),
+                ]
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20,bottom: 10,left: 25,right: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const SizedBox(
+                      width: 140,
+                      child: Text('Check your Groups',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.arrow_forward_sharp,
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                        )
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        items: [
+          BottomNavigationBarItem(
+              icon: const Icon(Icons.account_balance_wallet_sharp),
+              activeIcon: Image.asset('assets/images/wallet_active.png'),
+              label: '',
+          ),
+          BottomNavigationBarItem(
+              icon: Image.asset('assets/images/notification.png'),
+              activeIcon: const Icon(Icons.notifications_outlined),
+              label: '',
+          ),
+          BottomNavigationBarItem(
+              icon: Image.asset('assets/images/profile.png'),
+              activeIcon: const Icon(Icons.person_outline_sharp),
+              label: '',
+          ),
         ],
       ),
     );
